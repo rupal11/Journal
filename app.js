c6678afdc45e8209d9f53ad2dff391ffe8a9b4c9
rupal11 <rupaldas2001@gmail.com>
2021-08-03 16:06:40 +0530

    Linked with database,changed color-scheme

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose=require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/journalDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts=[];
+
+const postSchema = new mongoose.Schema({
+  title : String,
+  content : String
+});
 
 const app = express();
 
@@ -17,8 +25,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const Post = mongoose.model("Post",postSchema);
+
 app.get("/",function(req,res){
-res.render("home",{contentOfHome : homeStartingContent , postsArray : posts});
+
+  Post.find({},function(err,foundposts){
+    if(err)
+     console.log(err);
+    else
+    res.render("home",{contentOfHome : homeStartingContent , postsArray : foundposts});
+  })
+
 
 });
 
@@ -35,31 +52,32 @@ app.get("/compose",function(req,res){
   
 })
 
-app.get("/posts/:postName",function(req,res){
-  const requestedTitle=_.lowerCase(req.params.postName);
-  
-  posts.forEach(function(post){
+app.get("/posts/:id",function(req,res){
+  const requestedPostId=req.params.id;
 
-    const storedTitle=_.lowerCase(post.title)
-
-    if(requestedTitle===storedTitle){
+  Post.findOne( {_id:requestedPostId} , function(err,foundPost){
+    if(err){
+      console.log(err);
+    }
+    if(foundPost){
       res.render("post",{
-        title : post.title ,
-        body : post.body
+        title : foundPost.title ,
+        body : foundPost.content
       });
     }
-
-    });
+    
+  });
   
-})
+});
 
 app.post("/compose",function(req,res){
-  const post={
+ 
+  const post=new Post({
     title : req.body.composeTitle,
-    body : req.body.composeText
-  };
-  
-  posts.push(post);
+    content : req.body.composeText
+  });
+  // posts.push(post);
+  post.save();
   res.redirect("/");
 
 
